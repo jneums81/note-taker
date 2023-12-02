3ff77f0d49f347c200641e72fdc8b31e42ff23c9
Jackson Neuman <neumanjackson@gmail.com>
2023-12-02 13:27:52 -0600

    added api routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid'); // Import the uuid package
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,14 +15,40 @@ app.use(express.static('public'));
 
 // Your routes will go here
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-  });
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./develop/db/db.json', 'utf8')) || [];
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4(); // Generate a unique ID using uuid
   
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
+  const notes = JSON.parse(fs.readFileSync('./develop/db/db.json', 'utf8')) || [];
+  notes.push(newNote);
   
+  fs.writeFileSync('./develop/db/db.json', JSON.stringify(notes));
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  let notes = JSON.parse(fs.readFileSync('./develop/db/db.json', 'utf8')) || [];
+  notes = notes.filter(note => note.id !== noteId);
+
+  fs.writeFileSync('./develop/db/db.json', JSON.stringify(notes));
+  res.json({ success: true });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
+
